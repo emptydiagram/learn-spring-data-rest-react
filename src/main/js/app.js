2ef3125b06c3f33ba632913e595efba72a178a9b
maniacalhonks <maniacalhonks@gmail.com>
2019-08-11 15:31:44 -0400

    Fix react app

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -6,42 +6,42 @@ const client = require('./client');
 
 class App extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {employees: []};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {employees: []};
+  }
 
-	componentDidMount() {
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
-		});
-	}
+  componentDidMount() {
+    client({method: 'GET', path: '/api/employees'}).done(response => {
+      this.setState({employees: response.entity._embedded.employees});
+    });
+  }
 
-	render() {
-		return (
-			<EmployeeList employees={this.state.employees}/>
-		)
+  render() {
+    return (
+      <EmployeeList employees={this.state.employees}/>
+    )
   }
 }
 
 class EmployeeList extends React.Component {
-	render() {
-		const employees = this.props.employees.map(employee =>
-			<Employee key={employee._links.self.href} employee={employee}/>
-		);
-		return (
-			<table>
-				<tbody>
-					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Description</th>
-					</tr>
-					{employees}
-				</tbody>
-			</table>
-		)
-	}
+  render() {
+    const employees = this.props.employees.map(employee =>
+      <Employee key={employee._links.self.href} employee={employee}/>
+    );
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Description</th>
+          </tr>
+          {employees}
+        </tbody>
+      </table>
+    )
+  }
 }
 
 class Employee extends React.Component {
@@ -56,8 +56,7 @@ class Employee extends React.Component {
   }
 }
 
-
 ReactDOM.render(
   <App />,
-  documentGetElementById('react')
+  document.getElementById('react')
 )
