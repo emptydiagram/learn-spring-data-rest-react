f178ecb73c23bb9a9477f88eb8825c87b8f123e1
maniacalhonks <maniacalhonks@gmail.com>
2019-08-11 13:33:30 -0400

    Initial commit
    Spring Initializr (https://start.spring.io/) with
    
     - Spring Boot 2.1.7
     - deps:
        - Rest Repositories
        - Thymeleaf
        - Spring Data JPA
        - H2 Database

---
+++ src/main/java/party/afraidto/learnspringdatarestreact/LearnSpringDataRestReactApplication.java
@@ -0,0 +1,13 @@
+package party.afraidto.learnspringdatarestreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringDataRestReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringDataRestReactApplication.class, args);
+	}
+
+}
